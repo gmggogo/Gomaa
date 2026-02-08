ddc3672698d942cdfb8dec3f89f8151e2b3aa58c
Gomaa Hassanein <gmggogo2222@gmail.com>
2026-02-07 21:32:06 -0700

    fix render crash - replace bcrypt with bcryptjs

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
-
+const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
