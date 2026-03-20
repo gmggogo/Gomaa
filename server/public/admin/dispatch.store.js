01eb3b275e4364c168defe7b8c55f27ecfb9345b
Gomaa Hassanein <gmggogo2222@gmail.com>
2026-03-20 02:08:59 -0700

    fix trips route

--- server/public/admin/dispatch.store.js
+++ server/public/admin/dispatch.store.js
@@ -1,18 +1,33 @@
 const Store = {
 
 async getTrips(){
+try{
 const res = await fetch("/api/trips")
 return await res.json()
+}catch(e){
+console.log("Trips API Error", e)
+return []
+}
 },
 
 async getDrivers(){
+try{
 const res = await fetch("/api/users/driver")
 return await res.json()
+}catch(e){
+console.log("Drivers API Error", e)
+return []
+}
 },
 
 async getSchedule(){
+try{
 const res = await fetch("/api/driver-schedule")
 return await res.json()
+}catch(e){
+console.log("Schedule API Error", e)
+return {}
+}
 }
 
 }
