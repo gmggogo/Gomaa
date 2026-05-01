c7ffc0e5a20ab8a3b2d2dbe1dc7a9b815e010587
Gomaa Hassanein <gmggogo2222@gmail.com>
2026-04-30 18:14:40 -0700

    fix trips route

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.zoho.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
