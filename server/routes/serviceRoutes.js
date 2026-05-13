16d3a1508fe3cd8dd33cfb799a5e54ef8b0b7f2a
Gomaa Hassanein <gmggogo2222@gmail.com>
2026-05-13 15:33:54 -0700

    fix trips route

--- server/routes/serviceRoutes.js
+++ server/routes/serviceRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Service = require("../models/service.js");
+const Service = require("../models/Service");
 
 /* =========================
    GET SERVICES
