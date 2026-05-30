2fb6b154e5e80a03678d1554b1db11567ab1ecca
Gomaa Hassanein <gmggogo2222@gmail.com>
2026-05-30 05:06:27 -0700

    system design api

--- server/public/companies/review-core.js
+++ server/public/companies/review-core.js
@@ -295,14 +295,6 @@ function getServiceCodeFromTrip(trip){
 
 function getServiceByTrip(trip){
 
-alert(JSON.stringify({
-  tripNumber:trip.tripNumber,
-  tripType:trip.tripType,
-  type:trip.type,
-  isShared:trip.isShared,
-  passengers:Array.isArray(trip.passengers)
-},null,2));
-
   if(!trip) return null;
 
   const code = getServiceCodeFromTrip(trip);
