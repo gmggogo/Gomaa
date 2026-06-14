65dc25991ed9de22f099f720f90aa0de9a01747f
Gomaa Hassanein <gmggogo2222@gmail.com>
2026-06-13 20:26:27 -0700

    fix booking warning logic

--- server/routes/dispatchRoutes.js
+++ server/routes/dispatchRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const mongoose = require("mongoose");
 
-const Trip = mongoose.models.Trip;
+const Trip = require("../models/Trip");
 const User = require("../models/User");
 const DriverSchedule = require("../models/DriverSchedule");
 const DispatchAssignment = require("../models/DispatchAssignment");
